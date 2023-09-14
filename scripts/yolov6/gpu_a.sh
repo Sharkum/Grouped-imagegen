b64baac472f6e1e9a03e25600e502b52529102b7
Sharkum <sharan342.kumar@gmail.com>
2023-09-14 13:54:19 +0530

    added results for other models

--- scripts/yolov6/gpu_a.sh
+++ scripts/yolov6/gpu_a.sh
@@ -10,5 +10,5 @@
 #SBATCH --mail-type=ALL
 
 # ../.environment/bin/python3 tools/train.py --batch $batches --conf configs/yolov6l6.py --epochs $epochs --height 720 --width 1280 --data ./data_random_overlap.yaml --device 0 --name l6_model
-../.environment/bin/python3 tools/eval.py --data ./data_random_overlap.yaml --verbose --do_pr_metric True --batch $batches --height 720 --width 1280 --task test --weights runs/train/new_labels4/weights/best_ckpt.pt --device 0 --name new_labels_test
+../.environment/bin/python3 tools/eval.py --data ./data_random_overlap.yaml --verbose --do_pr_metric True --batch $batches --height 720 --width 1280 --task train --weights runs/train/new_labels4/weights/best_ckpt.pt --device 0 --name new_labels_test
 # ../.environment/bin/python3 tools/infer.py --yaml ./data_random_overlap.yaml --weights runs/train/new_labels3/weights/best_ckpt.pt --source ../random_overlapping_images/images/test/ --device 0
